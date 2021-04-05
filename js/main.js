0f3416f15a9868fd18bb320c9ca39754f2b37077
cyrox007 <cyrox007@gmail.com>
2021-04-05 11:35:39 +0300

    lazyLoad add

--- js/main.js
+++ js/main.js
@@ -45,3 +45,39 @@ modalClose.addEventListener('click', closeModal);
         });        
     }
 }
+
+const lazyImages = document.querySelectorAll('img[data-src],source[data-srcset]');
+const windowHeight = document.documentElement.clientHeight;
+
+
+
+let lazyImgPositions = [];
+if (lazyImages.length > 0) {
+    lazyImages.forEach(img => {
+        if (img.dataset.src || img.dataset.srcset) {
+            lazyImgPositions.push(img.getBoundingClientRect().top + pageYOffset);
+            lazyScrollCheck();
+        }
+    });
+}
+
+window.addEventListener('scroll', lazyScroll);
+
+function lazyScroll() {
+    if (document.querySelectorAll('img[data-src]').length > 0) {
+        lazyScrollCheck();
+    }
+}
+
+function lazyScrollCheck() {
+    let imgIndex = lazyImgPositions.findIndex(
+        item => pageYOffset > item - windowHeight
+    );
+    if (imgIndex >= 0) {
+        if (lazyImages[imgIndex].dataset.src) {
+            lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+            lazyImages[imgIndex].removeAttribute('data-src');
+        }
+        delete lazyImgPositions[imgIndex];
+    }
+}
